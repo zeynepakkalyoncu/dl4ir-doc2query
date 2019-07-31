80e5b20f73f44c52e6d3eff956944dd4a9c9f414
Zeynep Akkalyoncu Yilmaz <zakkalyoncu@uwaterloo.ca>
2019-07-31 18:09:30 -0400

    Update keyphrase expansion

--- expand/keyphrase.py
+++ expand/keyphrase.py
@@ -1,35 +1,54 @@
 import pke
 
+import sys
 import warnings
 warnings.filterwarnings("ignore")
 
 # initialize keyphrase extraction model
-extractor = pke.supervised.Kea()
+extractor = pke.unsupervised.YAKE()
 
 # load the content of the document, here document is expected to be in raw
 # format (i.e. a simple text file) and preprocessing is carried out using spacy
 
-with open('../msmarco_data/passages.tsv', 'r') as in_file, open('../msmarco_data/expanded/kea.txt', 'w') as out_file:
-    for line in in_file:
-        with open('temp.txt', 'r+', encoding='utf-7') as tmp_file:
-            tmp_file.write(line)
-            extractor.load_document(input='temp.txt', encoding='utf-8', language='en')
+for i in range(0, 8841824):
+    extractor.load_document(input='lines/passage.{}'.format(i), encoding='utf-8', language='en')
 
-            # keyphrase candidate selection, in the case of TopicRank: sequences of nouns
-            # and adjectives (i.e. `(Noun|Adj)*`)
-            extractor.candidate_selection()
+    # keyphrase candidate selection, in the case of TopicRank: sequences of nouns
+    # and adjectives (i.e. `(Noun|Adj)*`)
+    extractor.candidate_selection()
 
-            # print(extractor.__dict__)
+    # candidate weighting, in the case of TopicRank: using a random walk algorithm
+    extractor.candidate_weighting()
 
-            # candidate weighting, in the case of TopicRank: using a random walk algorithm
-            extractor.candidate_weighting()
+    # N-best selection, keyphrases contains the 10 highest scored candidates as
+    # (keyphrase, score) tuples
+    keyphrases = extractor.get_n_best(n=3)
 
-            # N-best selection, keyphrases contains the 10 highest scored candidates as
-            # (keyphrase, score) tuples
-            keyphrases = extractor.get_n_best(n=5)
+    # print(keyphrases)
 
-            print(keyphrases)
-            print('---')
+    expansion = ' '.join([kp[0] for kp in keyphrases]).strip()
+    out_file.write('{}\n'.format(expansion))
 
-            expansion = ' '.join([kp[0] for kp in keyphrases]).strip()
-            out_file.write('{}\n'.format(expansion))
+
+# with open(sys.argv[1], 'r') as in_file, open(sys.argv[2], 'w') as out_file:
+#     for line in in_file:
+#         with open(sys.argv[3], 'w', encoding='utf-8') as tmp_file:
+#             tmp_file.write(line)
+#         with open(sys.argv[3], 'r+', encoding='utf-8') as tmp_file:
+#             extractor.load_document(input=sys.argv[3], encoding='utf-8', language='en')
+#
+#             # keyphrase candidate selection, in the case of TopicRank: sequences of nouns
+#             # and adjectives (i.e. `(Noun|Adj)*`)
+#             extractor.candidate_selection()
+#
+#             # candidate weighting, in the case of TopicRank: using a random walk algorithm
+#             extractor.candidate_weighting()
+#
+#             # N-best selection, keyphrases contains the 10 highest scored candidates as
+#             # (keyphrase, score) tuples
+#             keyphrases = extractor.get_n_best(n=3)
+#
+#             # print(keyphrases)
+#
+#             expansion = ' '.join([kp[0] for kp in keyphrases]).strip()
+#             out_file.write('{}\n'.format(expansion))
